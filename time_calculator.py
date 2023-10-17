cf39bd22951c96c0e5cd5cc398bfd407bf356b88
ali-dev-uz <145695738+ali-dev-uz@users.noreply.github.com>
2023-10-17 07:52:21 +0000

    vaqt kalkulyatori

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,64 @@
-def add_time(start, duration):
+def add_time(start, duration, start_day=None):
+    # Define a dictionary to map days of the week to their indices.
+    days_of_week = {
+        "monday": 0,
+        "tuesday": 1,
+        "wednesday": 2,
+        "thursday": 3,
+        "friday": 4,
+        "saturday": 5,
+        "sunday": 6,
+    }
+
+    # Parse the start time, duration, and starting day.
+    start_time, period = start.split()
+    start_hour, start_minute = map(int, start_time.split(":"))
+    duration_hour, duration_minute = map(int, duration.split(":"))
+
+    # Calculate the total minutes from the start and duration.
+    total_minutes = start_hour * 60 + start_minute
+    total_minutes += duration_hour * 60 + duration_minute
+
+    # Calculate the resulting time and day.
+    new_hour, new_minute = divmod(total_minutes, 60)
+    new_period = period
+
+    # Handle the next day or multiple days later scenarios.
+    days_later = 0
+    while new_hour >= 12:
+        new_hour -= 12
+        if new_period == "PM":
+            days_later += 1
+        new_period = "AM"
+
+    # Determine the resulting day of the week, if provided.
+    if start_day is not None:
+        start_day = start_day.lower()
+        day_index = (days_of_week[start_day] + days_later) % 7
+        resulting_day = list(days_of_week.keys())[list(days_of_week.values()).index(day_index)]
+        resulting_day = resulting_day.capitalize()
+
+    # Build the result string.
+    result = f"{new_hour:02}:{new_minute:02} {new_period}"
+    if start_day is not None:
+        if days_later == 1:
+            result += f", {resulting_day} (next day)"
+        elif days_later > 1:
+            result += f", {resulting_day} ({days_later} days later)"
+        else:
+            result += f", {resulting_day}"
+    elif days_later == 1:
+        result += " (next day)"
+    elif days_later > 1:
+        result += f" ({days_later} days later)"
+
+    return result
 
 
-
-
-
-    return new_time
+# Examples of usage:
+print(add_time("3:00 PM", "3:10"))
+print(add_time("11:30 AM", "2:32", "Monday"))
+print(add_time("11:43 AM", "00:20"))
+print(add_time("10:10 PM", "3:30"))
+print(add_time("11:43 PM", "24:20", "tueSday"))
+print(add_time("6:30 PM", "205:12"))
